2135a3db415fcb07fbe37072ff4adb57a08e52c4
Ian Hubbertz <ian@fabmail.org>
2023-09-17 21:06:21 +0200

    Support for round variant of T-RGB
    ~ use latest version of TRGBArduinoSupport and thus TouchLib and thus
    support all T-RGB variants (oval, round, large round)

--- src/Singletons.cpp
+++ src/Singletons.cpp
@@ -13,7 +13,7 @@ BCLogger bclog;
 Statistics stats;
 FLClassicParser flparser(14, 2155);  // TODO: Load parameters from preferences
 UIFacade ui;
-TRGBSuppport trgb;
+//TRGBSuppport trgb;
 WifiWebserver webserver;
 BLEDevices bleDevs;
 
